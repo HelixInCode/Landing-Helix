584f0770bc205b63cb82b8e9bcc326589d64d9c4
Orlando Mendoza <ommv.17@gmail.com>
2020-05-16 02:32:15 -0400

    Reconfiguración de particulas

---
+++ js/smoot-scroll-custom.js
@@ -0,0 +1,7 @@
+var scroll = new SmoothScroll('a[href*="#"]', {
+
+	// Speed & Duration
+	speed: 500, // Integer. Amount of time in milliseconds it should take to scroll 1000px
+	speedAsDuration: true, // If true, use speed as the total duration of the scroll animation
+	offset: 50
+});
