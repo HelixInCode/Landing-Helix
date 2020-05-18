56bac27c0963d3f074881fbef878d46d9db683f4
Cin Sidan <cinthiaesidan_94@hotmail.com>
2020-05-18 16:44:43 -0300

    Agregué el código enviar.php y conecté el archivo con el formulario de contacto

---
+++ enviar.php
@@ -0,0 +1,30 @@
+<?php
+
+$nombre = $_REQUEST['nombre'];
+$telefono = $_REQUEST['telefono'];
+$email = $_REQUEST['correo'];
+$asunto = $_REQUEST['asunto'];
+$mensaje = $_REQUEST['mensaje'];
+
+
+
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Nombre de Cliente: " . $nombre . ",\r\n";
+$mensaje .= "Telefono: " . $telefono . ",\r\n";
+$mensaje .= "Mensaje: " . $mensaje . ",\r\n";
+
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'helixincode@gmail.com';
+$asunto = 'Mensaje enviado desde la página Helix';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:../index.html");
+?>
+
